2db6e86d93cfe290b924d2a23632b10bc61e9d68
nguyenvanty <ty.nguyenvan@hdwebsoft.co>
2021-12-02 18:33:35 +0700

    delete home

--- src/component/home/index.js
+++ src/component/home/index.js
@@ -2,7 +2,6 @@ const Home = () => {
   return (
     <div>
       <div>Home ne, 1</div>
-      <div>Home ne, hello world...</div>
       <div>Home ne, 2 tỷ</div>
       <div>Home ne, 50 tỷ</div>
       <div>Home ne, 10 tỷ</div>
